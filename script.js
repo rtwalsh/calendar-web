5216f27c022c2b7beeaf32ce3f047455d95bbd13
Robert Walsh <rwalsh@excalibur-solutions.com>
2024-09-11 09:56:00 -0400

    Add initialize function that runs when the page loads and shows the month and year in the console

--- script.js
+++ script.js
@@ -5,3 +5,15 @@
  *  Date:   September 10, 2024
  */
 
+let calendar;
+
+function initialize() {
+    let date = new Date();
+    calendar = {
+        month: date.getMonth(),
+        year: date.getFullYear()
+    }
+    
+    console.log("The current month is " + calendar.month);
+    console.log("The current year is " + calendar.year);
+}
